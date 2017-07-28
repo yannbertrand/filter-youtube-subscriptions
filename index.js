aef524e4288d08f8444ed786f01a07d3a12b2967
Yann Bertrand <yann.bertrand@orange.com>
2017-07-28 17:39:00 +0200

    Really hide videos

--- index.js
+++ index.js
@@ -11,5 +11,31 @@ function getFilters(upToDateFilters) {
 }
 
 function hideVideos() {
-  console.log(filters)
+  const lis = document.getElementsByClassName('yt-shelf-grid-item')
+  if (filters.length === 1 && filters[0].length === 0) {
+    for (const li of lis) {
+      if (li.style.display === 'none') {
+        const title = li.querySelector('h3 > a').innerHTML
+        console.log('Displaying "' + title + '"')
+        li.style.display = 'inline-block'
+      }
+    }
+
+    return
+  }
+
+  const videosToHide = filters.map(title => title.toUpperCase())
+  const videos = {}
+
+  for (const li of lis) {
+    const title = li.querySelector('h3 > a').innerHTML
+    const upperCaseTitle = title.toUpperCase()
+    if (new RegExp(videosToHide.join('|')).test(upperCaseTitle)) {
+      console.log('Hiding "' + title + '"')
+      li.style.display = 'none'
+    } else if (li.style.display === 'none') {
+      console.log('Displaying "' + title + '"')
+      li.style.display = 'inline-block'
+    }
+  }
 }
