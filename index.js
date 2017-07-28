80b87532b00cc1cb69e01e19b7e2691e5e92fc53
Yann Bertrand <yann.bertrand@orange.com>
2017-07-28 17:03:02 +0200

    Get the filters from the background script

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('filter-youtube-subscriptions')
+var browser = browser || chrome
+
+let filters = []
+
+browser.runtime.sendMessage('get-filters', getFilters)
+browser.runtime.onMessage.addListener(getFilters)
+
+function getFilters(upToDateFilters) {
+  filters = upToDateFilters
+  hideVideos()
+}
+
+function hideVideos() {
+  console.log(filters)
+}
